a053651fd7535e616685441a1696f8debee6982a
Samuel Key <s.key08@gmail.com>
2016-06-14 16:23:24 -0400

    presentable version

--- scripts.js
+++ scripts.js
@@ -1,12 +1,42 @@
 $(document).ready(function() {
 
 
-$('#menu').click(function(){
-  $('.about').toggle('slide', {direction : 'right'}, 500)
-  $('.work').toggle('slide', {direction : 'right'}, 700)
-  $('.contact').toggle('slide', {direction : 'right'}, 900)
-  $('.resume').toggle('slide', {direction : 'right'}, 1100)
-})
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+  $('#menu').click(function(){
+    $('.home').toggle('slide', {direction : 'right'}, 300);
+    $('.about').toggle('slide', {direction : 'right'}, 500);
+    $('.work').toggle('slide', {direction : 'right'}, 700);
+    $('.contact').toggle('slide', {direction : 'right'}, 900);
+    $('.resume').toggle('slide', {direction : 'right'}, 1100);
+    $('.fa-minus-square-o.menuicon').toggle();
+    $('.fa-plus-square-o.menuicon').toggle();
+  })
+
+  $('.images img').click(function() {
+    $('.overlay').show()
+    $('.appx').show()
+    $(this).siblings('.appinfo').show()
+  })
+
+  $('.overlay, .appx').click(function() {
+    $(this).hide()
+    $('.overlay').hide()
+    $('.appinfo').hide()
+  })
 
 
 })
