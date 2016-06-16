fa4cae4fd5c7ac31f885a8f15d447b00defed9bf
Samuel Key <s.key08@gmail.com>
2016-06-15 20:42:13 -0400

    work page fixes

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-
   $(function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -22,25 +21,25 @@ $(document).ready(function() {
     $('.work').toggle('slide', {direction : 'right'}, 700);
     $('.contact').toggle('slide', {direction : 'right'}, 900);
     $('.resume').toggle('slide', {direction : 'right'}, 1100);
-    $('.fa-minus-square-o.menuicon').toggle();
-    $('.fa-plus-square-o.menuicon').toggle();
+    $('.fa-minus-square-o.menuicon, .fa-plus-square-o.menuicon').toggle();
   })
 
   $('.images img, .viewmore').click(function() {
-    $('.overlay').show()
-    $('.appx').show()
+    $('.viewmore').hide()
+    $('.overlay, .appx').show()
     $(this).siblings('.appinfo').show()
     $('.project').css('position', 'inherit')
-    $('.viewmore').hide()
   })
 
   $('.overlay, .appx').click(function() {
-    $(this).hide()
-    $('.overlay').hide()
-    $('.appinfo').hide()
-    $('.project').css('position', 'relative')
     $('.viewmore').show()
+    $(this).hide()
+    $('.overlay, .appinfo').hide()
+    $('.project').css('position', 'relative')
   })
 
+  $('.images img').hover(function() {
+    $(this).siblings('.viewmore').toggle()
+  })
 
 })
